844c7baf43c5e1535759ad7a31cf594d71cb02a0
qkrsh0424 <qkrsh0424@naver.com>
2018-05-27 21:04:54 +0900

    submit lab8

--- functions.inc.php
+++ functions.inc.php
@@ -10,6 +10,20 @@ function generateLink($url, $label, $class) {
 
 function outputPostRow($number)  {
     include("travel-data.inc.php");
+		$row = '<div class="row">';
+		$row .= '<div class="col-md-4">';
+		$row .= '<a href="post.php?id='.${'postId'.$number}.'" class=""><img src="images/'.${'thumb'.$number}.'" alt="'.${'title'.$number}.'" class="img-responsive"/></a></div>';
+		$row .= '<div class="col-md-8">';
+		$row .= '<h2>'.${'title'.$number}.'</h2>';
+		$row .= '<div class="details">Posted by ';
+		$row .= generateLink('user.php?id='.${'userId'.$number}.'', ''.${'userName'.$number}.'', "");
+		$row .= '<span class="pull-right">'.${'date'.$number}.'</span>';
+		$row .= '<p class="ratings">';
+		$row .= constructRating(${'reviewsRating'.$number});
+		$row .= ' '.${'reviewsNum'.$number}.' REVIEWS</p></div>';
+		$row .= '<p class="excerpt">'.${'excerpt'.$number}.'</p>';
+		$row .= '<p><a href="post.php?id='.${'postId'.$number}.'" class="btn btn-primary btn-sm">Read more</a></p></div></div><hr/>';
+		echo $row;
 }
 
 /*
